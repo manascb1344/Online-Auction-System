e79e53767b45275062d4090d6681ec74082b0552
aarya-16 <bodasaarya@gmail.com>
2024-05-05 03:26:50 +0530

    Fixed some Overlap

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -99,7 +99,7 @@ const Nav = ({ header, socket }) => {
 									Auctions History
 								</button>
 								<button
-									className="px-4 py-2 bg-blue-500 text-white rounded-md"
+									className="px-4 py-2 bg-blue-500 text-white rounded-md mr-2"
 									onClick={() => navigate("/admin/transactions")}
 								>
 									Transaction History
